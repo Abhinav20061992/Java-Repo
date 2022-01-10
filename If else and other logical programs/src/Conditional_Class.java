7948c71dbbe6f8ef00b2c99aa5fa7a3f58ce6e8f
Abhinav20061992 <69417141+Abhinav20061992@users.noreply.github.com>
2022-01-10 23:10:14 +0530

    Add files via upload

---
+++ If else and other logical programs/src/Conditional_Class.java
@@ -0,0 +1,43 @@
+
+public class Conditional_Class {
+
+		public static void main(String[] args) {
+//Illustration of if-else			
+			int a=28;
+			int b=27;
+			
+			if (a==b) {
+				System.out.println("They are equal");				
+			}
+			else { System.out.println("They are not equal");}
+			
+//Illustration of for loop
+			for(int i=1;i<a;i++) {
+				System.out.println(i);
+			}
+			
+//Illustration of switch 			
+			 int number=20;  
+			      
+			    switch(number){  
+			    
+			    case 10: System.out.println("18");  
+			    break;  
+			    case 20: System.out.println("28");  
+			    break;  
+			    case 30: System.out.println("38");  
+			    break;  
+			    
+			    default:System.out.println("Not in 10, 20 or 30");  
+			    }  
+			    
+//Illustration of while loop			  
+			    
+			    int i=29;  
+			    while(i>=1){  
+			        System.out.println(i);  
+			    i--;  
+			    }  
+		}
+
+	}
